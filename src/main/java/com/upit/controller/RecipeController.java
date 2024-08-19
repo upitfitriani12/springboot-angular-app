ed135e6d76bfac5d1d6e943b16821fb273a8b583
upit <upit.upit@enigma.com>
2024-08-19 17:36:45 +0700

    update: endpoint recipe

--- src/main/java/com/upit/controller/RecipeController.java
+++ src/main/java/com/upit/controller/RecipeController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/recipes")
+@RequestMapping("/api/recipe")
 public class RecipeController {
 
     private final RecipeService recipeService;
